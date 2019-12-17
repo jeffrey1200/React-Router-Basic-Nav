730a7ae24338eda03335557bda307538572a144d
Jeffrey <jeffreymartespolanco@hotmail.com>
2019-12-16 16:57:33 -0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import {Switch,Route} from "react-router-dom"
 const App = () => (
-  <div>
-    <Navigation />
+  <div><Navigation /> 
+
+   <Switch>
+    <Route exact path = '/'>
+    <Home/>
+    </Route>
+    <Route path = '/about'>
+      <About/>
+    </Route>
+    <Route path = "/contact">
+      <Contact/>
+      </Route>
+</Switch>
   </div>
 );
 
